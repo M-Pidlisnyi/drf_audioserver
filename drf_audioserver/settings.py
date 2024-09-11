a484f3cfb94ed858906021c2318e4f3b3c744355
Mykhailo Pidlisnyi <mishapropatriot@gmail.com>
2024-09-11 14:06:04 +0300

    Update settings.py

--- drf_audioserver/settings.py
+++ drf_audioserver/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['drf-audioserver-e7dga8d7f8age2by.polandcentral-01.azurewebsites.net']
 
 
 # Application definition
